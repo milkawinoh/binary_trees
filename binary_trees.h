def1b073b7b5d7930f84a0a738893d86ce1cbf03
sophyakech <sophyaketch@gmail.com>
2023-11-02 18:49:34 -0700

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -38,4 +38,4 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+#endif
