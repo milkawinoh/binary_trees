ad6cc64dca6b71e47b3550008e8286096606500e
sophyakech <sophyaketch@gmail.com>
2023-11-02 18:59:09 -0700

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,12 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
